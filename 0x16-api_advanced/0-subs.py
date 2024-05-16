1d5ea9279d0212dbb1c5b26c8b85bb90f53a6aaa
headwurld <ihedpros@gmail.com>
2024-05-16 07:53:13 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,26 +7,11 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit."""
-    if not subreddit or not isinstance(subreddit, str):
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
         return 0
-
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': '0x16-api_advanced:v1.0.0 (by /u/efaeleng)'}
-
-    try:
-        response = requests.get(url, headers=headers)
-
-        # Check if the response status code is 200 (OK)
-        if response.status_code == 200:
-            data = response.json()
-            subscribers = data['data']['subscribers']
-            return subscribers
-        else:
-            print("Error:", response.status_code)
-            print("Response Content:", response.content.decode())
-            return 0  # Invalid subreddit or other error occurred
-
-    except requests.RequestException as e:
-        print("Request Exception:", e)
-        return 0  # Request failed or network error
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/efaeleng)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
