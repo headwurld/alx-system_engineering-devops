ca73ae8b76cb7cf4c278b4980a9e48ae95a74091
headwurld <ihedpros@gmail.com>
2024-05-16 07:48:33 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,11 +7,23 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
+    """Returns the number of subscribers for a given subreddit."""
+    if not subreddit or not isinstance(subreddit, str):
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/efaeleng)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': '0x16-api_advanced:v1.0.0 (by /u/efaeleng)'}
+
+    try:
+        response = requests.get(url, headers=headers)
+
+        # Check if the response status code is 200 (OK)
+        if response.status_code == 200:
+            data = response.json()
+            subscribers = data['data']['subscribers']
+            return subscribers
+        else:
+            return 0  # Invalid subreddit or other error occurred
+
+    except requests.RequestException:
+        return 0  # Request failed or network error
